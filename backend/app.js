78b254ff423fefb8b45cf1823e72dab86e967214
Lilia_Jemai <lilia.jemai@esprit.tn>
2026-01-20 13:28:30 +0100

    Change Port

--- backend/app.js
+++ backend/app.js
@@ -5,14 +5,9 @@ const app = express();
 app.use(express.json());
 
 app.get('/test-db', async (req, res) => {
-  try {
-    const result = await pool.query('SELECT NOW()');
-    res.json(result.rows[0]);
-  } catch (err) {
-    res.status(500).json({ error: err.message });
-  }
+ res.send('Hello Lilia ..!')
 });
 
-app.listen(5000, () => {
-  console.log('🚀 Serveur lancé sur http://localhost:5000');
+app.listen(process.env.PORT, () => {
+  console.log('🚀 Serveur lancé sur ', process.env.PORT);
 });
